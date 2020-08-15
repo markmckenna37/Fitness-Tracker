7315e02a159c226322920ef8a5c8eb562dbcf2a3
Mark <youremail@gmail.comg>
2020-08-14 19:44:48 -0600

    added api routes to exercise page

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
-const db = require("../models")
+const db = require("../models");
+const { Workout } = require("../models");
 
 module.exports = function (app) {
-app.post("/api/exercise", ({body}, res) => {
-    db.Workout.create(body)
+
+    app.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+app.post("/api/workouts", ({body}, res) => {
+    db.Workout.insertMany(body)
       .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { workout: _id } }, { new: true }))
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -13,4 +24,18 @@ app.post("/api/exercise", ({body}, res) => {
         res.json(err);
       });
   });
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
+
+
+
+
+
 }
